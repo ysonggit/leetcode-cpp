82fdcba97b3c85545b2085158e78cd78d91fe8ad
ysonggit <ysonggit@users.noreply.github.com>
2015-08-27 22:16:53 -0400

    Create P189RotateArray.cpp

---
+++ P189RotateArray.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(n<2 || k<0) return;
+        k = k%n;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
+    }
+};
