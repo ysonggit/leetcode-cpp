d49a86c61127b14337c3254bb9f3d6da20896d6a
ysonggit <ysonggit@users.noreply.github.com>
2015-09-08 21:40:58 -0700

    Create P042TrappingRainWater.cpp

---
+++ P042TrappingRainWater.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+       int n = height.size();
+       if(n<=2) return 0;
+       vector<int> left(n), right(n);
+       // mistake: initial values of left and right carefully! 
+       left[0] = height[0];
+       right[n-1] = height[n-1];
+       for(int i=1; i<n; i++){
+           left[i] = max(left[i-1], height[i]);
+       }
+       for(int i=n-2; i>=0; i--){
+           right[i] = max(right[i+1], height[i]);
+       }
+       int water = 0;
+       for(int i=1; i<n-1; i++){
+           water += min(left[i],right[i]) - height[i];
+       }
+       return water;
+    }
+};
