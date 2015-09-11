5719ca25cf973ab339b8f0b491e1c014de51f236
ysonggit <ysonggit@users.noreply.github.com>
2015-09-10 23:18:03 -0700

    Update P065ValidNumber.cpp

--- P065ValidNumber.cpp
+++ P065ValidNumber.cpp
@@ -1,74 +1,46 @@
 class Solution {
 public:
-    /*
-    最烦的题，没有之一！
-    return true if: ([ ]*)([+|-]*)([0.]*|[.]*)([0-9]+)([ ]*) or ([ ]*)([+|-]*)([0-9]+)([ ]*)([e|E1][0]+)
-    Use method http://leetcodenotes.wordpress.com/2013/11/23/leetcode-valid-number/
-    Match pattern :  AeB 
-    A could be numbers with/out DOT, with/out sign
-    B could be only integer numbers, with/out sign
-    AB must exist at the same time if e exists
-    */
-    // Match A pattern
-    bool isNumeric(string a){
-        // pass by the first bit. it could be +/-
-        if(a.length()>0 && (a[0] =='+' || a[0] =='-'))
-            a=a.substr(1);      
-        if (a.length() == 0) return false;
-        // check dot(s) by removing only one dot
-        // after removing, if there are still dots, return false
-        size_t dot_pos = a.find(string(".")); // return npos or first idx of .
-        if(dot_pos != string::npos){
-            a.erase(dot_pos, 1);
-            // if no char left after removing one ., then false
-            if(a.length()==0) return false; 
-        }
-        // check rest characters, must be all numeric characters
-        for(int i=0; i<a.length(); i++){
-            if(!isdigit(a[i])) 
-                return false;
-        }
-        return true;
-    }
-    // Match B pattern
-    bool isInteger(string b){
-        if(b.length() == 0 ) return false;
-        if(b[0] == '+' || b[0]=='-'){
-           b= b.substr(1);
-        }
-        // check if only -/+ stands
-        if(b.length()==0) return false;
-        for(int i = 0; i<b.length(); i++){
-            if(!isdigit(b[i])) 
-                return false;
-        }
-        return true;
-    }
-    // DAMN! Cpp is stupid!
     void trim(string & s){
-        int i=0, j= s.length()-1;
-        while(i<s.length() && s[i]==' ') i++;
-        while(j>=0 && s[j]==' ') j--;
-        s= s.substr(i, j-i+1);
+        int n= s.length();
+        int i=0, j=n-1;
+        while(i<n && s[i]==' ') i++;
+        while(j>=0 && s[j]==' ')j--;
+        s = s.substr(i, j-i+1);
     }
-
+    
     bool isNumber(string s) {
         trim(s);
-        int N = s.length();
-        if(N<=0) return false;
-        // 2. check whether e exists
-        size_t e_pos = s.find(string("e"));
-        // 3e or e9 is invalid
-        if(e_pos==0 ||e_pos == N-1 ) return false;
-        if(e_pos != string::npos){
-            // must match both A & B patterns
-            string a = s.substr(0, e_pos);
-            string b = s.substr(e_pos+1);
-            return isNumeric(a) && isInteger(b);
-        }else{
-            //no e, match pattern A
-            return isNumeric(s);
+        int i=0, n = s.length();
+        // if starts from a sign
+        if(s[i]=='+'||s[i]=='-') i++;
+        bool is_num = false;
+        // if starts/follows a number
+        while(i<n && isdigit(s[i])){
+            i++;
+            is_num = true;
         }
-        return false;
+        //if starts/follows a dot
+        if(i<n && s[i]=='.'){
+            i++;
+            while(i<n && isdigit(s[i])){
+                i++;
+                is_num = true;
+            }
+        }
+        // if follows an 'e'
+        // if starts from an e then false e9
+        if(is_num && i<n && s[i]=='e'){
+            // if ends with an e also false  1e
+            is_num = false;
+            // if any number follows e
+            i++;
+            // a sign after e is valid : " 005047e+6"
+            if(i<n && (s[i]=='+'|| s[i]=='-')) i++;
+            while(i<n && isdigit(s[i])){
+                i++;
+                is_num = true;
+            }
+        }
+        return is_num && i==n;
     }
 };
