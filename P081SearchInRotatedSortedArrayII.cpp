1c5f5b2b9667d19773eb9ab1d9cd258216ca4d1f
ysonggit <ysonggit@users.noreply.github.com>
2015-09-10 23:59:05 -0700

    Update P081SearchInRotatedSortedArrayII.cpp

--- P081SearchInRotatedSortedArrayII.cpp
+++ P081SearchInRotatedSortedArrayII.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool binarySearch(vector<int> & nums, int target, int low, int high){
-        if(low>=high) return false;
+        if(low>high) return false;
         int mid = low+(high-low)/2;
         if(nums[mid]==target) return true;
-        bool left = binarySearch(nums, target, low, mid);
+        bool left = binarySearch(nums, target, low, mid-1);
         bool right = binarySearch(nums, target, mid+1, high);
         return left ? left : right;
     }
     bool search(vector<int>& nums, int target) {
-        return binarySearch(nums, target, 0, nums.size());
+        return binarySearch(nums, target, 0, nums.size()-1);
     }
 };
