312533d4d70a1875cef3f00ae09493534f9263ad
ysonggit <ysonggit@users.noreply.github.com>
2015-09-15 22:27:29 -0700

    Update P078Subsets.cpp

--- P078Subsets.cpp
+++ P078Subsets.cpp
@@ -3,23 +3,20 @@ public:
     void dfs(vector<vector<int>> & res, vector<int> & nums, 
             vector<int> sol, int cur_idx)
     {
-        if(cur_idx==nums.size()){
+        for(int i=cur_idx; i<nums.size(); i++){
+            int cur = nums[i];
+            sol.push_back(cur);
             res.push_back(sol);
-            return;
-        }                
-        // ignore this one
-        dfs(res, nums, sol, cur_idx+1);
-        int cur = nums[cur_idx];
-        // put this one into subset
-        sol.push_back(cur);
-        dfs(res, nums, sol, cur_idx+1);
-        sol.pop_back();
+            dfs(res, nums, sol, i+1);
+            sol.pop_back();
+        }
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
         if(nums.size()==0) return res;
         vector<int> sol;
         sort(nums.begin(), nums.end());
+        res.push_back(sol);
         dfs(res, nums, sol, 0);
         return res;
     }
