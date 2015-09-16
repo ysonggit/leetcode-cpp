a9568d5ea49aebc9e29bfe63e0402d46a6b9cc08
ysonggit <ysonggit@users.noreply.github.com>
2015-09-15 23:16:24 -0400

    Create P046Permutations.cpp

---
+++ P046Permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void dfs(vector<vector<int> > & res, vector<int> & nums,
+            vector<int> sol, vector<int> visited)
+    {
+        if(sol.size()==nums.size()){
+            res.push_back(sol);
+            return;
+        }                
+        for(int i=0; i<nums.size(); i++){
+            if(visited[i]==0){
+                visited[i]=1;
+                sol.push_back(nums[i]);
+                dfs(res, nums, sol, visited);
+                sol.pop_back();
+                visited[i]=0;
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int> > res;
+        int n = nums.size();
+        if(n==0) return res;
+        sort(nums.begin(), nums.end());
+        vector<int> sol;
+        vector<int> visited(n, 0);
+        dfs(res, nums, sol, visited);
+        return res;
+    }
+};
