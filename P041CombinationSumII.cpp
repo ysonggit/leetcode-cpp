ea0e084ca0f7345d9c8a2328bda4e3a2f04dc232
ysonggit <ysonggit@users.noreply.github.com>
2015-09-15 20:05:10 -0400

    Create P041CombinationSumII.cpp

---
+++ P041CombinationSumII.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void dfs(vector<vector<int> >& res, vector<int>& candidates, vector<int> sol, int target, int cur_idx){
+        if(target==0){
+            res.push_back(sol);
+            return;
+        }
+        if(cur_idx>=candidates.size() || target < 0) return;
+        for(int i=cur_idx; i<candidates.size(); i++){
+            if(i> cur_idx && candidates[i]==candidates[i-1]) continue; // mistake : candidates[i]==candidates[cur_idx]
+            int cur = candidates[i];
+            sol.push_back(cur);
+            dfs(res, candidates, sol, target-cur, i+1);
+            sol.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int> > res;
+        vector<int> sol;
+        sort(candidates.begin(), candidates.end());
+        dfs(res, candidates, sol, target, 0);
+        return res;
+    }
+};
