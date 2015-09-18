fa5b14ceb49010fac7c91333686980e79adcebf8
ysonggit <ysonggit@users.noreply.github.com>
2015-09-17 20:32:57 -0700

    Update P010RegularExpressionMatching.cpp

--- P010RegularExpressionMatching.cpp
+++ P010RegularExpressionMatching.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     bool isMatch(const char *s, const char *p) {
         if(*p=='\0') return *s == '\0';
@@ -23,5 +23,64 @@ public:
     }
     bool isMatch(string s, string p){
         return isMatch(s.c_str(), p.c_str());
+    }
+};
+
+class Solution2 {
+public:
+    /**
+    * '*' match 0 or more preceding sequence
+    */
+    bool isMatch(string s, string p) {
+        const int n = s.length(), m = p.length();
+        if(m==0) return n==0;
+        bool ** D = new bool*[n+1];
+        for(int i=0; i<=n; i++){
+            D[i] = new bool[m+1];
+            for(int j=0; j<=m; j++){
+                D[i][j] = false;
+            }
+        }
+        D[0][0] = true;
+        D[0][1] = (p[0]=='*');
+        // initialize D[0][j] = D[0][j-2] if p[j-1]=='*'
+        for(int j=2; j<=m; j++){
+            if(p[j-1]=='*') D[0][j] = D[0][j-2];
+        }
+        for(int i=1; i<=n; i++){
+            char sc = s[i-1];
+            for(int j=1; j<=m; j++){
+                char pc = p[j-1];
+                if(pc!='*'){
+                    D[i][j] = (D[i-1][j-1] && (sc==pc|| pc=='.'));
+                }else{
+                    // check preceding char in p, if . or not
+                    if(j>=2){
+                        if(p[j-2]=='.'){
+                            // similar to wildcard matching but consider one more previous step
+                            if(D[i][j-1] || D[i][j-2]){
+                                for(int k=i; k<=n; k++) D[k][j] = true;
+                            }
+                        }else{
+                            D[i][j] = D[i][j-1] || D[i][j-2];
+                            // also consider one more previous step of s
+                            if(i>=2){
+                                D[i][j] = D[i][j] || (D[i-1][j] && sc==s[i-2] && s[i-2] == p[j-2]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if(false){
+            for(int i=0; i<=n; i++){
+                for(int j=0; j<=m; j++){
+                    if(D[i][j]) cout<<"1 |";
+                    else cout<<"0 |";
+                }
+                cout<<endl;
+            }
+        }
+        return D[n][m];
     }
 };
