d41e8053bab8b560b95cd2c5964d270fe0435a65
ysonggit <ysonggit@users.noreply.github.com>
2015-09-17 23:40:41 -0700

    Update P045JumpGameII.cpp

--- P045JumpGameII.cpp
+++ P045JumpGameII.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
 // fucking corner cases [0] [1] []
     int jump(vector<int>& nums) {
@@ -19,5 +19,31 @@ public:
             if(furthest_pos_with_minstep >=n-1) return min_steps;
         }
         return min_steps;
+    }
+};
+
+class Solution2 {
+public:
+    // test case [2    3    1 1 4]
+    //      0|0   1|2  2|4  
+    //           [2   0    1  0 1]
+    //      0|0   1|2 3|3  4|3
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int lower = 0, upper = 0;
+        int steps = 0;
+        while(upper< n-1){
+            int pre_lower = lower;
+            int pre_upper = upper;
+            for(int pos=pre_lower; pos<=pre_upper; pos++){
+                // within current bounded range, find the next upperbound
+                upper = max(nums[pos]+pos, upper);
+            }
+            // next lowerbound <==> move 1 step forward
+            lower = pre_upper+1;
+            if(lower > upper) return 0; // never reach last
+            steps++;
+        }
+        return steps;
     }
 };
