ad7040a07a65d4cac9a086495078a497a3844e08
ysonggit <ysonggit@users.noreply.github.com>
2015-09-18 23:44:13 -0700

    Update P055JumpGame.cpp

--- P055JumpGame.cpp
+++ P055JumpGame.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        if(n<=0) return true;
-        int furthest_pos_sofar = 0;// mistake : donot initialize to nums[0], since given [0] should return true
-        for(int i=0; i<n; i++){
-            if(i>furthest_pos_sofar) return false;
-            int furthest_pos_fromhere = i+nums[i];
-            furthest_pos_sofar = max(furthest_pos_sofar, furthest_pos_fromhere);
-            if(furthest_pos_sofar >=n-1){
-                return true;
-            }
+        int max_reachable = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(max_reachable<i) return false;
+            max_reachable = max(max_reachable, i+nums[i]);
         }
-        return false;
+        return true;
     }
 };
