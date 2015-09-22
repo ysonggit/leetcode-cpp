ce548f9e6f46c0da710e13c9b64b672ddac9c64a
ysonggit <ysonggit@users.noreply.github.com>
2015-09-22 16:35:56 -0700

    Create P088MergeSortedArray.cpp

---
+++ P088MergeSortedArray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1, j = n-1;
+        for(int k = m+n-1; k>=0; k--){
+            if(i>=0 && j>=0){
+                nums1[k] = (nums2[j] > nums1[i] ) ? nums2[j--] : nums1[i--]; 
+            }else if(i>=0){
+                nums1[k] = nums1[i--];
+            }else{
+                nums1[k] = nums2[j--];
+            }
+        }
+    }
+};
