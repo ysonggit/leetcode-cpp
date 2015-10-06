e311ab5bec61a0dd7a07ac7ba02281aef2cdbcad
ysonggit <ysonggit@users.noreply.github.com>
2015-10-06 19:42:51 -0400

    Update P158ReadNCharactersGivenRead4II.cpp

--- P158ReadNCharactersGivenRead4II.cpp
+++ P158ReadNCharactersGivenRead4II.cpp
@@ -9,21 +9,21 @@ public:
      * @return    The number of characters read
      */
     char buffer [4];
-    int offset =0, bufsize = 0;
+    int offset =0, bytes_from_src = 0;
     int read(char *buf, int n) {
         bool eof = false;
         int bytes_copied = 0;
         while(bytes_copied< n && !eof){
-            if(bufsize==0){
-                bufsize = read4(buffer);
-                eof = bufsize < 4;
+            if(bytes_from_src==0){
+                bytes_from_src = read4(buffer);
+                eof = bytes_from_src< 4;
             }
-            int bytes_to_copy = min(n-bytes_copied, bufsize);
+            int bytes_to_copy = min(n-bytes_copied, bytes_from_src);
             //memcpy( void* dest, const void* src, std::size_t count );
             memcpy(buf+bytes_copied, buffer+offset, bytes_to_copy);
             offset = (offset+bytes_to_copy)%4;
             bytes_copied += bytes_to_copy;
-            bufsize -= bytes_to_copy;
+            bytes_from_src -= bytes_to_copy;
         }
         return bytes_copied;
     }
