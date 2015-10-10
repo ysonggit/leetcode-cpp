5ba0857f2687e1b45440aa7dc21e98d23757fec6
ysonggit <ysonggit@users.noreply.github.com>
2015-10-10 10:25:45 -0700

    Update P291WordPatternII.cpp

--- P291WordPatternII.cpp
+++ P291WordPatternII.cpp
@@ -2,31 +2,40 @@ class Solution {
 public:
     bool isMatch(string & pattern, string & str, 
                 int start_pattern_idx, int start_str_idx,
-                unordered_map<char, string> forward_mapping){
+                unordered_map<char, string> forward_mapping,
+                unordered_map<string, char> backward_mapping){
         if(start_pattern_idx >= pattern.length() && start_str_idx>= str.length())
             return true;
         if(start_pattern_idx >= pattern.length() || start_str_idx >= str.length())
             return false;
         char pc = pattern[start_pattern_idx];
-        bool inserted = false;
+        bool inserted_forward = false, inserted_backward = false;
         for(int len=1; len<=str.length()-start_str_idx; len++){
             string s = str.substr(start_str_idx, len);
             if(forward_mapping.find(pc)==forward_mapping.end()){
                 forward_mapping[pc] = s;
-                inserted = true;
+                inserted_forward = true;
             }else{
-                if(forward_mapping[pc]!=s) continue; // mistake: never return false here
+                if(forward_mapping[pc]!=s) continue;
             }
-            if(isMatch(pattern, str, start_pattern_idx+1, start_str_idx+len, forward_mapping)){
+            if(backward_mapping.find(s)==backward_mapping.end()){
+                backward_mapping[s] = pc;
+                inserted_backward = true;
+            }else{
+                if(backward_mapping[s]!=pc) return false;
+            }
+            if(isMatch(pattern, str, start_pattern_idx+1, start_str_idx+len, forward_mapping, backward_mapping)){
                 return true;
             }
-            if(inserted) forward_mapping.erase(pc);
+            if(inserted_forward) forward_mapping.erase(pc);
+            if(inserted_backward) backward_mapping.erase(s);
         }
         return false;
     }
     bool wordPatternMatch(string pattern, string str) {
         if(pattern.empty()) return str.empty();
         unordered_map<char, string> forward_mapping;
-        return isMatch(pattern, str, 0, 0, forward_mapping);
+        unordered_map<string, char> backward_mapping;
+        return isMatch(pattern, str, 0, 0, forward_mapping, backward_mapping);
     }
 };
