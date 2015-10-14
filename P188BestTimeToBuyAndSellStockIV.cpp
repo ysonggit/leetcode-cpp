402b1ed45966b98d6dc423717c3b7b18ee62bbd1
ysonggit <ysonggit@users.noreply.github.com>
2015-10-13 19:28:50 -0700

    Update P188BestTimeToBuyAndSellStockIV.cpp

--- P188BestTimeToBuyAndSellStockIV.cpp
+++ P188BestTimeToBuyAndSellStockIV.cpp
@@ -15,13 +15,10 @@ public:
         }
         vector<vector<int>> D(n, vector<int>(k+1, 0));
         for(int j=1; j<=k; j++){
-            int max_diff = D[0][j-1] - prices[0]; // mistake: initial value can not be 0
+            int local_max = D[0][j-1] - prices[0];
             for(int i=1; i<n; i++){
-                // for h in [0,i-1] find max_of_(D[h][j]-prices[h])
-                for(int h=0; h<i; h++){
-                    max_diff = max(D[h][j-1]-prices[h], max_diff);
-                }
-                D[i][j] = max(D[i-1][j], prices[i]+max_diff);
+                D[i][j] = max(D[i-1][j], prices[i]+local_max);
+                local_max = max(local_max, D[i][j-1] - prices[i]);
             }
         }
         return D[n-1][k];
