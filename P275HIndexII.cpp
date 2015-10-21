f183f48f424e9d73f2a9198f6c6e245edd5ad7e8
ysonggit <ysonggit@users.noreply.github.com>
2015-10-20 18:50:21 -0700

    Update P275HIndexII.cpp

--- P275HIndexII.cpp
+++ P275HIndexII.cpp
@@ -4,27 +4,15 @@ public:
     int hIndex(vector<int>& citations) {
         int n = citations.size();
         if(n==0) return 0;
-        int h_idx = 0;
-        /**
-         * naive O(n) solution
-            for(int i=0; i<n; i++){
-                h_idx = max(h_idx, min(citations[i], n-i));
-            }
-         */
         int front = 0, back = n-1;
         while(front<=back){
             int mid = (front+back)/2;
-            int cur_cites = citations[mid]; 
-            if(cur_cites == n-mid){
-                return cur_cites;
-            }else if(cur_cites > n-mid){
-                h_idx = n-mid;
-                --back;
+            if(citations[mid] >= n-mid){ // mistake >= not >
+                back = mid-1;
             }else{
-                h_idx = cur_cites;
-                ++front;
+                front = mid+1;
             }
         }
-        return h_idx;
+        return n-front;
     }
 };
