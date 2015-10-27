1022b5132fbe33185020f27d03f2d5f976860203
ysonggit <ysonggit@users.noreply.github.com>
2015-10-27 00:33:59 -0700

    Update P069Sqrt.cpp

--- P069Sqrt.cpp
+++ P069Sqrt.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     // Binary search and high is always converged to the one that 1 larger than the result.
     // return high-1
@@ -13,3 +13,21 @@ public:
         return high-1;
     }
 };
+
+public class Solution2 {
+    public int mySqrt(int x) {
+        if(x<2) return x;
+        int low = 0, high = x;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(x/mid >= mid && x/(mid+1) < mid+1){
+                return mid;
+            }else if(x/mid < mid){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+}
