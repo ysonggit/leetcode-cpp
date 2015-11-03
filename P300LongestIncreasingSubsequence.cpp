48dcebe96ff706740669af98487bbd5c11a940e7
ysonggit <ysonggit@users.noreply.github.com>
2015-11-02 22:30:29 -0500

    Create P300LongestIncreasingSubsequence.cpp

---
+++ P300LongestIncreasingSubsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        if(n<2) return n;
+        vector<int> D(n, 0);
+        D[0] = 1;
+        int max_len = 1;
+        for(int i=1; i<n; i++){
+            D[i] = 1;
+            for(int j=0; j<i; j++){
+                if(nums[i]>nums[j]) D[i] = max(D[i], D[j]+1);
+            }
+            max_len = max(max_len, D[i]);
+        }
+        return max_len;
+    }
+};
