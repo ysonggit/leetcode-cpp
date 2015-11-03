b592b2531df0db11be4e5f4846383c84cf4e764c
ysonggit <ysonggit@users.noreply.github.com>
2015-11-02 20:45:08 -0500

    Update P215KthLargestElementInAnArray.cpp

--- P215KthLargestElementInAnArray.cpp
+++ P215KthLargestElementInAnArray.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     // minheap O(k) space, O(nlogk) time
     int findKthLargest(vector<int>& nums, int k) {
@@ -15,5 +15,18 @@ public:
             }
         }
         return minheap.top();
+    }
+};
+
+class Solution2 {
+public:
+    // maxheap O(n) space O(klogn + n) time
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(n==0) return 0;
+        priority_queue<int, vector<int>, std::less<int>> maxheap; // can use make_heap
+        for(int i : nums) maxheap.push(i);
+        for(int i=0; i<k-1; i++) maxheap.pop();
+        return maxheap.top();
     }
 };
