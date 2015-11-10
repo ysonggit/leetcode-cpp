3242fbaa830029fe367f01c2cdf8d2b707db9fb0
ysonggit <ysonggit@users.noreply.github.com>
2015-11-10 10:56:20 -0800

    Update P227BasicCalculatorII.cpp

--- P227BasicCalculatorII.cpp
+++ P227BasicCalculatorII.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     // from StefanPochmann (https://leetcode.com/discuss/41641/17-lines-c-easy-20-ms)
     // key1: istringstream and head/tail safeguards: '+'
@@ -19,6 +19,40 @@ public:
                 if(op == '*') num *= mul;
                 else num /= mul;
             }
+        }
+        return res;
+    }
+};
+
+class Solution2 {
+public: // assume no overflow
+    int calculate(string s) {
+        stack<int> operands;
+        int res = 0, num = 0;
+        int sign = '+'; // sign before a number
+        for(char c : (s+"+")){
+            if(c==' ') continue;
+            if(isdigit(c)) num = num*10 + (int)(c-'0');
+            else{
+                if(sign=='+') operands.push(num);
+                if(sign=='-') operands.push(-num);
+                if(sign=='*') {
+                    int mul = operands.top() * num;
+                    operands.pop();
+                    operands.push(mul);
+                }
+                if(sign=='/'){
+                    int div = operands.top() / num;
+                    operands.pop();
+                    operands.push(div);
+                }
+                sign = c;
+                num = 0;
+            }
+        }
+        while(!operands.empty()) {
+            res += operands.top();
+            operands.pop(); 
         }
         return res;
     }
