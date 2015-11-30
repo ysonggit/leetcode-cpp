b6a8a44328297948d2b450f5e6b2da80b96b2d68
ysonggit <ysonggit@users.noreply.github.com>
2015-11-29 22:39:51 -0500

    Update P311SparseMatrixMultiplication.cpp

--- P311SparseMatrixMultiplication.cpp
+++ P311SparseMatrixMultiplication.cpp
@@ -8,8 +8,13 @@ public:
         res.resize(m, vector<int>(k, 0));
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                for(int l=0; l<k; l++){
-                    if(A[i][j]!=0) res[i][l] += A[i][j] * B[j][l];
+                //for(int l=0; l<k; l++){
+                //    if(A[i][j]!=0) res[i][l] += A[i][j] * B[j][l];
+                //}
+                if(A[i][j]!=0) {
+                    for(int l=0; l<k; l++){
+                        res[i][l] += A[i][j] * B[j][l];
+                    }
                 }
             }
         }
